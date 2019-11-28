ca8555278f9313abf761fa24bcdc6884ab955f81
ola <dashevska.olga@gmail.com>
2019-11-28 03:45:10 +0200

    add modal-window and adaptive for mobile

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,7 @@
 $('.phoneidcl').inputmask('+38(999) 999-99-99',{ "oncomplete": function(){
         $(".disbutt").prop("disabled", false);
     }, "onincomplete": function(){ $(".disbutt").prop("disabled", true); } });
+
+$(document).ready(function() {
+    $('.popup-link').magnificPopup();
+});
